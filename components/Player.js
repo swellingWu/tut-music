8a04a7810bd0e4e669aaca33cf833758a4211412
nep <3368939477@qq.com>
2022-05-10 16:01:29 +0800

    5.10 first
    Signed-off-by: nep <3368939477@qq.com>

---
+++ components/Player.js
@@ -0,0 +1,14 @@
+import React from "react";
+function Component() {
+  return (
+    <div className="c-player">
+      <audio></audio>
+      <h4>Playing now</h4>
+      <p>
+        <strong>Next up: </strong>
+        Kindest Regards by Witt Lowry
+      </p>
+    </div>
+  );
+}
+export default Component;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Player from "../components/Player";
 
 function App() {
+  const [songs, setSongs] = useState([
+    {
+      title: "想去海边",
+      artist: "🎉 夏日入侵企画",
+      img_src: "../images/log.jpg",
+      src: "../music/想去海边.mp3",
+    },
+    {
+      title: "夏目漱石",
+      artist: "🎉 橘子海",
+      img_src: "../images/log.jpeg",
+      src: "../music/夏目漱石.mp3",
+    },
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player song={songs[currentSongIndex]} nextSong={songs[nextSongIndex]} />
     </div>
   );
 }
