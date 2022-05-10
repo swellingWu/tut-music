7060dd0bd533db5a2b5479d05e74b4bd5e43f119
nep <3368939477@qq.com>
2022-05-10 19:37:45 +0800

    5.10
    Signed-off-by: nep <3368939477@qq.com>

--- src/App.js
+++ src/App.js
@@ -1,44 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import Player from "./components/Player";
 
 function App() {
-  const [songs, setSongs] = useState([
+  const [songs] = useState([
     {
       title: "想去海边",
       artist: "🎉 夏日入侵企画",
       img_src:
-        "http://p2.music.126.net/sLWN-iePq4ESOMPER0IWgQ==/109951164602081973.jpg?param=130y130",
-      src: "../music/想去海边.mp3",
+        "https://www.xzmp3.com/down/45af8fd19ed9.mp3",
+      src: "./music/夏目漱石.mp3",
     },
     {
       title: "极恶都市",
       artist: "🎉 夏日入侵企画",
       img_src:
-        "https://y.qq.com/music/photo_new/T002R300x300M000002rVsxO1OCAu1_1.jpg?max_age=2592000",
-      src: "../music/想去海边.mp3",
+        "https://www.xzmp3.com/down/4e89440764ba.mp3",
+      src: "./music/想去海边.mp3",
     },
     {
       title: "夏目漱石",
       artist: "🎉 橘子海",
       img_src:
-        "https://y.qq.com/music/photo_new/T002R300x300M000001goINS2qAQS6_1.jpg?max_age=2592000",
-      src: "../music/夏目漱石.mp3",
+        "https://www.xzmp3.com/down/4e89440764ba.mp3",
+      src: "./music/夏目漱石.mp3",
     },
     {
       title: "人生浪费指南",
       artist: "🎉 夏日入侵企画",
       img_src:
-        "https://qpic.y.qq.com/music_cover/iaVNVUXOg73ua71Z0dtuz7fMwxyZvNkXvqt4GnUc7BoPw249DSr8JIw/300",
-      src: "../music/想去海边.mp3",
+        "https://www.xzmp3.com/down/45af8fd19ed9.mp3",
+      src: "./music/想去海边.mp3",
     },
   ]);
 
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
+
   return (
     <div className="App">
-      <Player song={songs[currentSongIndex]} nextSong={songs[nextSongIndex]} />
+      <Player currentSongIndex={currentSongIndex} setCurrentSongIndex={setCurrentSongIndex} nextSongIndex={nextSongIndex} songs={songs} />
     </div>
   );
 }
